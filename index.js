5d0b2e5622a10740a70c6ca5a3309aa10709dc13
Luis Salazar <luis.salazar@sr-consultores.com>
2020-06-10 20:48:31 -0400

    initial commit

---
+++ index.js
@@ -0,0 +1,15 @@
+/**** subtree test *****/
+const actores = [{
+  nombre: 'Jack',
+  apellido: 'Nicholson',
+  ocupacion: 'Actor'
+},
+{
+  nombre: 'Kate',
+  apellido: 'Winslet',
+  ocupacion: 'Actriz'
+}]
+
+for (const actor of actores) {
+  console.log(actor);
+}
